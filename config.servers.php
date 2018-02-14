b48cde09a008478963ddcd64377680779d176067
Bash <jerwin@aiming-inc.com.ph>
2018-02-14 14:49:13 +0800

    Update config.servers.php
    Changed hostname

--- config.servers.php
+++ config.servers.php
@@ -3,7 +3,7 @@
 $i = 1;
 
 $cfg['Servers'][$i]['verbose'] = 'w1-dbm01';
-$cfg['Servers'][$i]['host'] = 'w1-db';
+$cfg['Servers'][$i]['host'] = 'w1-db-658fb54bc9';
 $cfg['Servers'][$i]['port'] = '';
 $cfg['Servers'][$i]['socket'] = '';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
@@ -14,7 +14,7 @@ $cfg['Servers'][$i]['AllowNoPassword'] = false;
 $i++;
 
 $cfg['Servers'][$i]['verbose'] = 'w2-dbm01';
-$cfg['Servers'][$i]['host'] = 'w2-db';
+$cfg['Servers'][$i]['host'] = 'w2-db-6fd6b75568';
 $cfg['Servers'][$i]['port'] = '';
 $cfg['Servers'][$i]['socket'] = '';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
@@ -25,7 +25,7 @@ $cfg['Servers'][$i]['AllowNoPassword'] = false;
 $i++;
 
 $cfg['Servers'][$i]['verbose'] = 'w3-dbm01';
-$cfg['Servers'][$i]['host'] = 'w3-db';
+$cfg['Servers'][$i]['host'] = 'w3-db-5c69fbb6b9';
 $cfg['Servers'][$i]['port'] = '';
 $cfg['Servers'][$i]['socket'] = '';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
